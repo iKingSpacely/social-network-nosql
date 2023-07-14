e15f2fd98e31ff9c2fd395749ea5197eafa816ae
Adam Colin <accolin89@gmail.com>
2023-07-13 21:02:55 -0500

    added the schema formats from the readme into the models.

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,25 +1,51 @@
 const { Schema, Types, model } = require('mongoose');
 
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        responseBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }
+);
+
+const thoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280,
+        minLength: 1,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema]
+});
+
+postSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
+  });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const Thoughts = model('reactionCount', thoughtSchema);
 
 module.exports = Thoughts;
--- models/User.js
+++ models/User.js
@@ -1,26 +1,28 @@
 const { Schema, model } = require('mongoose');
 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trimmed: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, "Must be a valid email"],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'thought'}],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'user'}],
+});
+
+postSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
+  });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const User = model('user', userSchema);
 
 module.exports = User;
 
